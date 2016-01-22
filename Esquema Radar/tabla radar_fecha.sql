a1e332af7e540b074d68a82a73852def9e7eb98b
jespindola <jespindola@gesecology.com>
2016-01-22 12:45:21 -0300

    tablas

---
+++ Esquema Radar/tabla radar_fecha.sql
@@ -0,0 +1,15 @@
+﻿-- Table: radar_terrestre.radar_fecha
+
+-- DROP TABLE radar_terrestre.radar_fecha;
+
+CREATE TABLE radar_terrestre.radar_fecha
+(
+  id_radar double precision NOT NULL,
+  fecha timestamp without time zone NOT NULL,
+  desplazamiento double precision
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE radar_terrestre.radar_fecha
+  OWNER TO postgres;
---
+++ Esquema Radar/tabla registro_radar.sql
@@ -0,0 +1,29 @@
+﻿-- Table: radar_terrestre.registro_radar
+
+-- DROP TABLE radar_terrestre.registro_radar;
+
+CREATE TABLE radar_terrestre.registro_radar
+(
+  id_registro serial NOT NULL,
+  fecha_registro timestamp without time zone,
+  id_radar bigint,
+  nombre_tabla character varying(50),
+  fecha_radar timestamp without time zone,
+  indicador_uso character varying(1),
+  CONSTRAINT registro_radar_pkey PRIMARY KEY (id_registro)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE radar_terrestre.registro_radar
+  OWNER TO postgres;
+
+-- Index: radar_terrestre.registro_radar_ind_idx
+
+-- DROP INDEX radar_terrestre.registro_radar_ind_idx;
+
+CREATE INDEX registro_radar_ind_idx
+  ON radar_terrestre.registro_radar
+  USING btree
+  (indicador_uso COLLATE pg_catalog."default");
+
